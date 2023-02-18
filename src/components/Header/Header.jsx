7ade34e914004ec22c9fd2cc51162a2c3928b3f9
Diego G <diegoar.guz@hotmail.com>
2023-02-18 11:28:20 -0500

    Version 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Header.css'
+import Logo1 from '../../assets/Logo1.png'
 
 function Header() {
   return (
-    <div className="header">Header</div>
+    <div className="header">
+      <img src={Logo1} alt="Energia fit" className='logo'/>
+      <ul className='header-menu'>
+        <li>Home</li>
+        <li>Productos</li>
+        <li>Nosotros</li>
+        <li>Recetas</li>
+        <li>Testimonios</li>
+      </ul>
+    </div>
   )
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
 import Header from '../Header/Header'
+import heart from '../../assets/heart.png'
 import './Hero.css'
 
 function Hero() {
   return (
     <div className='hero'>
-        <div className="left-h">
+        <div className="left">
             <Header/>
+            <div className="best">
+              <div></div>
+              <span>Hábitos saludables que marcan la diferencia</span>
+            </div>
+            <div className="text">
+               <div>
+                <span className='stroke-text'>alimenta </span>
+                <span>tu</span>
+               </div>
+               <div><span>bienestar</span></div>
+               <div><span>Amor propio es elegir tu bienestar a través de una alimentación sana y consciente</span></div>
+            </div>
+            <div className="figures">
+              <div>
+                <span>+20</span>
+                <span>marcas</span>
+              </div>
+              <div>
+                <span>+100</span>
+                <span>productos</span>
+              </div>
+              <div>
+                <span>Y muchas</span>
+                <span>recetas</span>
+              </div>
+            </div>
+            <div className="buttons">
+              <button className='btn'>Get start</button>
+              <button className='btn'>Learn More</button>
+            </div>
         </div>
-        <div className="right-h">right side</div>
+        <div className="right">
+          <button className="btn">Join Us</button>
+          <div className="heart">
+            <img src={heart} alt="heart rate" />
+            <span>Heart Rate</span>
+            <span>116 bpm</span>
+          </div>
+        </div>
     </div>
   )
 }
